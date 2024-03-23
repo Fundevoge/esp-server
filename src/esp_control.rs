0d93cbebfbb7490f3df396f54e8c93861ba4c7ed
Fundevoge <59122389+Fundevoge@users.noreply.github.com>
2024-03-23 21:23:11 +0100

    Revert "added keepalive timeout to esp tcp streams"
    This reverts commit 36ae38dafdb2b93bf543717d5812430052f3f618.

--- src/esp_control.rs
+++ src/esp_control.rs
@@ -27,9 +27,7 @@ pub async fn esp_stream_controller() -> anyhow::Result<()> {
 
     loop {
         let (stream, _) = tcp_listener.accept().await?;
-        let stream = make_tcp_stream_keepalive(stream)?;
         println!("Handling stream...");
-
         if let Err(e) = handle_esp_stream_connection(stream).await {
             println!("[ESP] Stream Connection ended with error: {e}");
         }
@@ -63,17 +61,9 @@ pub async fn esp_state_controller() -> anyhow::Result<()> {
 
     loop {
         let (stream, _) = tcp_listener.accept().await?;
-        let stream = make_tcp_stream_keepalive(stream)?;
         println!("Handling state control...");
-        if let Some(last_task_handle) = last_handle {
-            if last_task_handle.is_finished() {
-                if let Err(e) = last_task_handle.await? {
-                    println!("[ESP] State Control Connection ended with error: {e}");
-                }
-            } else {
-                last_task_handle.abort();
-                println!("[ESP] State Control Aborted stale connection");
-            }
+        if let Err(e) = handle_esp_state_connection(stream).await {
+            println!("[ESP] State Control Connection ended with error: {e}");
         }
 
         last_handle = Some(tokio::spawn(handle_esp_state_connection(stream)));
@@ -104,17 +94,9 @@ pub async fn esp_time_controller() -> anyhow::Result<()> {
 
     loop {
         let (stream, _) = tcp_listener.accept().await?;
-        let stream = make_tcp_stream_keepalive(stream)?;
         println!("Handling time control...");
-        if let Some(last_task_handle) = last_handle {
-            if last_task_handle.is_finished() {
-                if let Err(e) = last_task_handle.await? {
-                    println!("[ESP] Time Control Connection ended with error: {e}");
-                }
-            } else {
-                last_task_handle.abort();
-                println!("[ESP] Time Control Aborted stale connection");
-            }
+        if let Err(e) = handle_esp_time_connection(stream).await {
+            println!("[ESP] Time Control Connection ended with error: {e}");
         }
 
         last_handle = Some(tokio::spawn(handle_esp_time_connection(stream)));
@@ -134,18 +116,4 @@ async fn handle_esp_time_connection(mut tcp_stream: TcpStream) -> anyhow::Result
             .await?;
         sleep(Duration::from_secs(15)).await;
     }
-}
-
-fn make_tcp_stream_keepalive(
-    stream: tokio::net::TcpStream,
-) -> anyhow::Result<tokio::net::TcpStream> {
-    let stream: std::net::TcpStream = stream.into_std().unwrap();
-    let socket: socket2::Socket = socket2::Socket::from(stream);
-    let keepalive = socket2::TcpKeepalive::new()
-        .with_time(Duration::from_secs(4))
-        .with_interval(Duration::from_secs(2))
-        .with_retries(4);
-    socket.set_tcp_keepalive(&keepalive)?;
-    let stream: std::net::TcpStream = socket.into();
-    Ok(tokio::net::TcpStream::from_std(stream).unwrap())
 }
